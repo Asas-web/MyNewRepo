36e0203b7c6f587210cc4f51353a4e6f61935a79
Asas-web <ahmad_1472016@outlook.sa>
2022-11-05 07:55:32 +0200

    create new project

---
+++ script.js
@@ -0,0 +1,14 @@
+var hide = document.querySelector("#Cookiess");
+var temp1 = document.querySelector("#s1");
+
+function Showalert() {
+  alert("Loading Weather Report");
+}
+function HideElement() {
+  hide.remove();
+}
+
+function ChangeTheTemp() {
+  temp1 = 0.9 * 25 + 32;
+  console.log(temp1);
+}
